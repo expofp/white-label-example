256c2a3dd3f718cd2df2425fe34fca29ee878146
SergLyss <serge@expofp.com>
2023-03-15 15:54:54 +0300

    add example fp, login page, validation send Data.js

--- server.js
+++ server.js
@@ -1,43 +1,68 @@
 import express from "express";
-import path from "path";
 import ejs from "ejs";
 import { signWithScopeSecret } from "./crypto.js";
 import * as dotenv from "dotenv";
+import session from 'express-session';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app = express();
+app.use(bodyParser.json({ type: 'application/json'}));
+app.use(cookieParser());
+
+app.use(session({
+	secret: 'white-label-example',
+	resave: false,
+	saveUninitialized: false
+}));
+
 app.engine('html', ejs.renderFile);
+app.set('view engine', 'html');
 
-const __dirname = path.resolve();
-
-const expo = "22";
+const expo = "CoffeeFest";
 
 const SCOPE = process.env.SCOPE;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 const signature = await signWithScopeSecret({ expo }, PRIVATE_KEY);
 
+app.use(express.static("public"));
+
+app.use(requireAuthentication);
+
+app.get("/login", (req, res) => {
+	res.render("pages/login");
+});
+
+app.post('/login', (req, res) => {
+	const { username, password } = req.body;
+	if (username === process.env.USERNAME && password === process.env.PASSWORD) {
+		req.session.authenticated = true;
+		res.redirect('/');
+	} else {
+		res.status(401).send('Incorrect username or password');
+	}
+});
+
 app.get("/designer", (req, res) => {
-	res.render(path.join(__dirname, "designer.html"), { signature, SCOPE, expo });
+	res.render("pages/designer", { signature, SCOPE, expo });
 });
 
 app.get("/", (req, res) => {
-	res.send(`
-		<div>
-			<a href='/designer'>Designer</a>
-		</div>
-		<div>
-			<a href='/sendData'>Send data.js</a>
-		</div>
-	`);
-});
-
-app.get("/sendData", (req, res) => {
-	res.render(path.join(__dirname, "sendData.html"), { signature, SCOPE, expo });
+	res.render("pages/index", { signature, SCOPE, expo });
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log(`Server started on port ${PORT}`);
 });
+
+function requireAuthentication(req, res, next) {
+	if (req.session.authenticated || req.path === '/login') {
+		next();
+	} else {
+		res.redirect('/login');
+	}
+}
